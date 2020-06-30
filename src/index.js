148414ffbbae0c9edc4285ed9b6519dcdb21022d
molko <ya.komarovkirill@yandex.ru>
2020-06-30 16:10:22 +0300

    React.StrictMode delete

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
     <Router>
       <App />
-    </Router>
-  </React.StrictMode>,
+    </Router>,
   document.getElementById("root")
 );
