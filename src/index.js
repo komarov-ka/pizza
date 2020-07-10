3dd1942937dc4dc338ed0cec9fd98193fbc437f6
molko <ya.komarovkirill@yandex.ru>
2020-07-10 22:53:29 +0300

    added redux, provider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux'
 
+import store from "./redux/store"
 
-import './scss/app.scss'
-
-import App from './App';
+import "./scss/app.scss";
+import App from "./App";
 
 
 ReactDOM.render(
-    <Router>
+  <Router>
+    <Provider store={store}>
       <App />
-    </Router>,
+    </Provider>
+  </Router>,
   document.getElementById("root")
 );
